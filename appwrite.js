4d8c3af9fc7aff3fa9bb699806bb26935f6f1c7f
Mazhar <mazhar@latitude-e7440.localdomain>
2025-05-08 21:18:24 +0500

    first commit

--- appwrite.js
+++ appwrite.js
@@ -4,6 +4,7 @@ const PROJECT_ID = process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID;
 const DATABASE_ID = process.env.NEXT_PUBLIC_APPWRITE_DATABASE_ID;
 const COLLECTION_ID = process.env.NEXT_PUBLIC_APPWRITE_COLLECTION_ID;
 
+
 const client = new Client()
   .setEndpoint("https://cloud.appwrite.io/v1")
   .setProject(PROJECT_ID);
